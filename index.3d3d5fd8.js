cdd05000ceba5b2caf33925e2447db3317f124e1
Yuri Gatskovskyi <yurimoreau@gmail.com>
2024-07-16 11:11:46 +0300

    make build

---
+++ index.3d3d5fd8.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".tree");document.querySelectorAll(".tree li").forEach(function(e){var t=document.createElement("span");e.prepend(t),t.prepend(t.nextSibling)}),e.addEventListener("click",function(e){if("SPAN"===e.target.tagName){var t=e.target.nextSibling;"UL"===t.tagName&&(t.hidden=!t.hidden)}});
+//# sourceMappingURL=index.3d3d5fd8.js.map
